03f1add527bbabd9edf27e764e02b63c549c4ac6
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2023-02-03 17:33:35 +0400

    1

--- homework1/task_1.py
+++
@@ -1,5 +0,0 @@
-name = input("Введите ваше имя: ")
-password = input("Введите ваш password: ")
-age = input("Введите ваш возраст: ")
-print(f"Ваши данные для входа в аккаунт: имя - {name}, "
-      f"пароль - {password}, возраст - {age}")
--- homework1/task_3.py
+++
@@ -1,4 +0,0 @@
-n = input("Введите число: ")
-nn = n*2
-nnn = n*3
-print(f"n+nn+nnn = {n}+{nn}+{nnn} = {int(n)+int(nn)+int(nnn)}")
